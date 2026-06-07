f193e32ccd648202ad78b739c0f34052d056628c
Ben Ngie <katheka@gmail.com>
2026-06-07 16:41:38 -0400

    Add files via upload

---
+++ assets/js/cms-config.js
@@ -0,0 +1,6 @@
+window.RSA_CMS_CONFIG = {
+  // Paste the deployed Google Apps Script Web App URL here after launch.
+  // Public site reads and registration posts will reuse this endpoint.
+  PUBLIC_API_URL: 'https://script.google.com/macros/s/AKfycbyEFeyDAPkPqWUJ-FMmMzAofbrzV0HsKXZygO_70pOeh4ecHubT8eu1NDJTNh-sETyO/exec',
+  SITE_ID: 'rising-stars-atlanta'
+};
