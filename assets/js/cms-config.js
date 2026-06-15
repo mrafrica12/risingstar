0cdb914062ba17719697facc5464631ac77fceb7
Ben Ngie <katheka@gmail.com>
2026-06-15 14:22:17 -0400

    Delete assets directory

--- assets/js/cms-config.js
+++
@@ -1,6 +0,0 @@
-window.RSA_CMS_CONFIG = {
-  // Paste the deployed Google Apps Script Web App URL here after launch.
-  // Public site reads and registration posts will reuse this endpoint.
-  PUBLIC_API_URL: 'https://script.google.com/macros/s/AKfycbxcn3vfdh1UHyOTuEdJGZXT3mc0_aRCDBl3U7doBEJ_eeMkDolW4hsgCgFORQmQD6i4/exec',
-  SITE_ID: 'rising-stars-atlanta'
-};
